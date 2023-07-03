b19f04250588c35312262fa6533b14784c8d064b
Ojaswi Chopra <99067527+ojaswichopra@users.noreply.github.com>
2023-07-03 21:52:06 +0530

    Update naive.cpp

--- naive.cpp
+++ naive.cpp
@@ -9,113 +9,38 @@ using namespace std;
 typedef unordered_map<string, string> Circuit;
 
 // Function to parse the circuit file and create the circuit representation
-Circuit parseCircuitFile(const string &circuitFile)
+Circuit parseCircuitFile(const string &ipFile)
 {
     Circuit circuit;
 
-    ifstream file(circuitFile);
+    ifstream file(ipFile);
     if (!file)
     {
-        cout << "Failed to open circuit file: " << circuitFile << endl;
+        cout << "Failed to open circuit file: " << ipFile << endl;
         return circuit;
     }
 
     string line;
     while (getline(file, line))
     {
-        // Remove spaces from the expression
+        // Remove spaces from the logicExpression
         line.erase(remove(line.begin(), line.end(), ' '), line.end());
         size_t pos = line.find('=');
         if (pos != string::npos)
         {
             string node = line.substr(0, pos);
-            string expression = line.substr(pos + 1);
+            string logicExpression = line.substr(pos + 1);
 
-            // Store the expression as-is in the circuit representation
-            circuit[node] = expression;
+            // Store the logicExpression as-is in the circuit representation
+            circuit[node] = logicExpression;
         }
     }
 
     return circuit;
 }
 
-// Function to evaluate the logical expression for a given node in the circuit
-int evaluateExpression(const Circuit &circuit, const string &node, vector<int> &inputVector,unordered_map<string,int> &m)
-{
-    if(m.find(node)!=m.end()) return m[node];
-    if (circuit.find(node) != circuit.end())
-    {
-        string expression = circuit.at(node);
-
-        if (expression[0] == '~')
-        {
-            // NOT gate
-            int result = evaluateExpression(circuit, expression.substr(1), inputVector,m);
-            return m[node]=!result;
-        }
-        else if (expression.find('&') != string::npos)
-        {
-            // AND gate
-            size_t pos = expression.find('&');
-            string operand1 = expression.substr(0, pos);
-            string operand2 = expression.substr(pos + 1);
-            int result1 = evaluateExpression(circuit, operand1, inputVector,m);
-            int result2 = evaluateExpression(circuit, operand2, inputVector,m);
-            return m[node]=result1 && result2;
-        }
-        else if (expression.find('|') != string::npos)
-        {
-            // OR gate
-            size_t pos = expression.find('|');
-            string operand1 = expression.substr(0, pos);
-            string operand2 = expression.substr(pos + 1);
-            int result1 = evaluateExpression(circuit, operand1, inputVector,m);
-            int result2 = evaluateExpression(circuit, operand2, inputVector,m);
-            return m[node]=result1 || result2;
-        }
-        else if (expression.find('^') != string::npos)
-        {
-            // XOR gate
-            size_t pos = expression.find('^');
-            string operand1 = expression.substr(0, pos);
-            string operand2 = expression.substr(pos + 1);
-            int result1 = evaluateExpression(circuit, operand1, inputVector,m);
-            int result2 = evaluateExpression(circuit, operand2, inputVector,m);
-            return m[node]=result1 ^ result2;
-        }
-        else if (expression == "1" || expression == "0")
-        {
-            // Fault Site
-            return m[node]=stoi(expression);
-        }
-    }
-    else
-    {
-        // Input nodes
-        if (node == "A")
-            return m[node]=inputVector[0]; // Input A is 1
-        else if (node == "B")
-            return m[node]=inputVector[1]; // Input B is 0
-        else if (node == "C")
-            return m[node]=inputVector[2]; // Input C is 1
-        else if (node == "D")
-            return m[node]=inputVector[3]; // Input D is 1
-    }
-
-    cout << "Invalid node: " << node << endl;
-    return m[node]=-1;
-}
-
-// Function to evaluate the circuit and get the output value
-int evaluateCircuit(const Circuit &circuit, vector<int> &inputVector)
-{
-    unordered_map<string,int> m;
-    string outputNode = "Z";
-    return evaluateExpression(circuit, outputNode, inputVector,m);
-}
-
 // Function to generate all possible input vectors
-vector<vector<int>> generateInputVectors(int numInputs)
+vector<vector<int>> generate(int numInputs)
 {
     vector<vector<int>> inputVectors;
     int totalCombinations = pow(2, numInputs);
@@ -135,30 +60,89 @@ vector<vector<int>> generateInputVectors(int numInputs)
     return inputVectors;
 }
 
-// Function to apply the fault at the given node
-void applyFault(Circuit &circuit, const string &faultNode, const string &faultType)
+// Function to evaluate the logical logicExpression for a given node in the circuit
+int evalExpression(const Circuit &circuit, const string &node, vector<int> &inputVector, unordered_map<string, int> &m)
 {
-    if (faultType == "SA0")
+    if (m.find(node) != m.end())
+        return m[node];
+    if (circuit.find(node) != circuit.end())
     {
-        circuit[faultNode] = "0";
-    }
-    else if (faultType == "SA1")
-    {
-        circuit[faultNode] = "1";
+        string logicExpression = circuit.at(node);
+
+        if (logicExpression[0] == '~')
+        {
+            // NOT gate
+            int result = evalExpression(circuit, logicExpression.substr(1), inputVector, m);
+            return m[node] = !result;
+        }
+        else if (logicExpression.find('&') != string::npos)
+        {
+            // AND gate
+            size_t pos = logicExpression.find('&');
+            string operand1 = logicExpression.substr(0, pos);
+            string operand2 = logicExpression.substr(pos + 1);
+            int result1 = evalExpression(circuit, operand1, inputVector, m);
+            int result2 = evalExpression(circuit, operand2, inputVector, m);
+            return m[node] = result1 && result2;
+        }
+        else if (logicExpression.find('|') != string::npos)
+        {
+            // OR gate
+            size_t pos = logicExpression.find('|');
+            string operand1 = logicExpression.substr(0, pos);
+            string operand2 = logicExpression.substr(pos + 1);
+            int result1 = evalExpression(circuit, operand1, inputVector, m);
+            int result2 = evalExpression(circuit, operand2, inputVector, m);
+            return m[node] = result1 || result2;
+        }
+        else if (logicExpression.find('^') != string::npos)
+        {
+            // XOR gate
+            size_t pos = logicExpression.find('^');
+            string operand1 = logicExpression.substr(0, pos);
+            string operand2 = logicExpression.substr(pos + 1);
+            int result1 = evalExpression(circuit, operand1, inputVector, m);
+            int result2 = evalExpression(circuit, operand2, inputVector, m);
+            return m[node] = result1 ^ result2;
+        }
+        else if (logicExpression == "1" || logicExpression == "0")
+        {
+            // Fault Site
+            return m[node] = stoi(logicExpression);
+        }
     }
     else
     {
-        cout << "Invalid fault type: " << faultType << endl;
+        // Input nodes
+        if (node == "A")
+            return m[node] = inputVector[0]; // Input A is 1
+        else if (node == "B")
+            return m[node] = inputVector[1]; // Input B is 0
+        else if (node == "C")
+            return m[node] = inputVector[2]; // Input C is 1
+        else if (node == "D")
+            return m[node] = inputVector[3]; // Input D is 1
     }
+
+    cout << "Invalid node: " << node << endl;
+    return m[node] = -1;
 }
 
-// Function to print the input vector and expected output to the output file
-void printOutput(const vector<int> &inputVector, int expectedOutput, const string &outputFile)
+// Function to evaluate the circuit and get the Op value
+int evalCircuit(const Circuit &circuit, vector<int> &inputVector)
+{
+    unordered_map<string, int> m;
+    string outputNode = "Z";
+    return evalExpression(circuit, outputNode, inputVector, m);
+}
+
+// Function to print the input vector and expected Op to the Op file
+void printToOutput(const vector<int> &inputVector, int expectedOp, const string &outputFile)
 {
     ofstream file(outputFile);
     if (!file)
     {
-        cout << "Failed to open output file: " << outputFile << endl;
+        cout << "Failed to open Op file: " << outputFile << endl;
         return;
     }
 
@@ -169,7 +153,7 @@ void printOutput(const vector<int> &inputVector, int expectedOutput, const strin
     }
     file << "], ";
 
-    file << "Z = " << expectedOutput << endl;
+    file << "Z = " << expectedOp << endl;
 
     file.close();
 }
@@ -177,7 +161,7 @@ void printOutput(const vector<int> &inputVector, int expectedOutput, const strin
 int main()
 {
     // Circuit file and fault details
-    string circuitFile = "input.txt";
+    string ipFile = "input.txt";
     string faultNode;
     string faultType;
 
@@ -188,30 +172,41 @@ int main()
     cin >> faultType;
 
     // Parse the circuit file
-    Circuit circuit = parseCircuitFile(circuitFile);
+    Circuit circuit = parseCircuitFile(ipFile);
 
     // Generate all possible input vectors
-    vector<vector<int>> inputVectors = generateInputVectors(4);
+    vector<vector<int>> inputVectors = generate(4);
 
     // Iterate through each input vector
     for (auto inputVector : inputVectors)
     {
         // Apply the fault at the given node
         Circuit faultyCircuit = circuit;
-        applyFault(faultyCircuit, faultNode, faultType);
+        if (faultType == "SA0")
+        {
+            faultyCircuit[faultNode] = "0";
+        }
+        else if (faultType == "SA1")
+        {
+            faultyCircuit[faultNode] = "1";
+        }
+        else
+        {
+            cout << "Invalid fault type: " << faultType << endl;
+            return 0;
+        }
 
-        // Simulate the circuit with fault-free inputs and record the expected output value
-        
-        int expectedOutput = evaluateCircuit(circuit, inputVector);
+        // Simulate the circuit with fault-free inputs and record the expected Op value
+        int expectedOp = evalCircuit(circuit, inputVector);
 
         // Simulate the circuit with the candidate input vector and the fault applied
-        int output = evaluateCircuit(faultyCircuit, inputVector);
+        int Op = evalCircuit(faultyCircuit, inputVector);
 
-        // Check if the output value matches the expected output value
-        if (output != expectedOutput)
+        // Check if the Op value matches the expected Op value
+        if (Op != expectedOp)
         {
-            // Print the potential input vector and the expected output to the output file
-            printOutput(inputVector, output, "output.txt");
+            // Print the potential input vector and the expected Op to the Op file
+            printToOutput(inputVector, Op, "output.txt");
             return 0;
         }
     }
@@ -219,7 +214,7 @@ int main()
     ofstream file("output.txt");
     if (!file)
     {
-        cout << "Failed to open output file: output.txt" << endl;
+        cout << "Failed to open Op file: Op.txt" << endl;
         return 0;
     }
     file << "Given fault is ATPG untestable fault" << endl;
